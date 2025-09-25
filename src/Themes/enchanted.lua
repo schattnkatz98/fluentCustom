07b99e289025e77cb2bd973365276bdbdd40c177
katzenschatten98 <116763385+schattnkatz98@users.noreply.github.com>
2025-09-25 10:41:00 +0200

    Create enchanted.lua

---
+++ src/Themes/enchanted.lua
@@ -0,0 +1,43 @@
+return {
+         Name = 'Enchanted',
+        Accent = Color3.fromRGB(90, 235, 190),
+        AcrylicMain = Color3.fromRGB(22, 52, 60),
+        AcrylicBorder = Color3.fromRGB(32, 78, 90),
+        AcrylicGradient = ColorSequence.new(Color3.fromRGB(18, 70, 95), Color3.fromRGB(70, 30, 110)),
+        AcrylicNoise = 0.92,
+        TitleBarLine = Color3.fromRGB(44, 100, 112),
+
+        Tab = Color3.fromRGB(70, 110, 115),
+        Element = Color3.fromRGB(64, 104, 110),
+        ElementBorder = Color3.fromRGB(30, 66, 74),
+        InElementBorder = Color3.fromRGB(52, 92, 102),
+        ElementTransparency = 0.86,
+
+        ToggleSlider = Color3.fromRGB(90, 235, 190),
+        ToggleToggled = Color3.fromRGB(0, 0, 0),
+        SliderRail = Color3.fromRGB(110, 240, 200),
+
+        DropdownFrame = Color3.fromRGB(38, 80, 88),
+        DropdownHolder = Color3.fromRGB(22, 52, 60),
+        DropdownBorder = Color3.fromRGB(18, 42, 50),
+        DropdownOption = Color3.fromRGB(58, 100, 106),
+
+        Keybind = Color3.fromRGB(64, 104, 110),
+        Input = Color3.fromRGB(58, 96, 102),
+        InputFocused = Color3.fromRGB(24, 22, 40),
+        InputIndicator = Color3.fromRGB(175, 220, 210),
+
+        Dialog = Color3.fromRGB(30, 72, 78),
+        DialogHolder = Color3.fromRGB(24, 60, 66),
+        DialogHolderLine = Color3.fromRGB(20, 52, 58),
+        DialogButton = Color3.fromRGB(96, 230, 190),
+        DialogButtonBorder = Color3.fromRGB(20, 52, 58),
+        DialogBorder = Color3.fromRGB(26, 58, 64),
+        DialogInput = Color3.fromRGB(58, 96, 102),
+        DialogInputLine = Color3.fromRGB(165, 205, 195),
+
+        Text = Color3.fromRGB(240, 240, 240),
+        SubText = Color3.fromRGB(188, 198, 198),
+        Hover = Color3.fromRGB(86, 136, 140),
+        HoverChange = 0.04
+	}
